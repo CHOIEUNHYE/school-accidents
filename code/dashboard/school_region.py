7fed51433a06c2be7da57d9c642f2790b4b790d7
hxxzn <100256848+hxxzn@users.noreply.github.com>
2024-07-21 13:18:02 +0900

    0721 streamlit

--- code/dashboard/school_region.py
+++ code/dashboard/school_region.py
@@ -13,7 +13,7 @@ import json
 from function_하윤 import *  # function.py 파일에서 모든 함수 불러오기 
 
 # geojson파일 불러오기
-file_path = '../../data/지도 시각화 데이터\/L_SCCO_CTPRVN.json'
+file_path = '../../data/지도 시각화 데이터\TL_SCCO_CTPRVN.json'
 geojson = gpd.read_file(file_path)
 geojson = geojson.replace('강원도','강원특별자치도') # 강원도 표기 변경
 geojson['지역'] = geojson['CTP_KOR_NM'].apply(extract_region) # 학교 안전사고 데이터와 지역 표기 형식 맞추기(충청남도->충북)
@@ -47,11 +47,11 @@ def create_map(df, location_column, parameter_column):
     return fig
 
 # 학교별 데이터 불러오기 - 시도별
-school2019 = pd.ExcelFile('../../data/학교,학과별 데이터셋/2019년_상반기_시도별.xlsx')
-school2020 = pd.ExcelFile('../../data/학교,학과별 데이터셋/2020년_상반기_시도별.xlsx')
-school2021 = pd.ExcelFile('../../data/학교,학과별 데이터셋/2021년_상반기_시도별.xlsx')
-school2022 = pd.ExcelFile('../../data/학교,학과별 데이터셋/2022년_상반기_시도별.xlsx')
-school2023 = pd.ExcelFile('../../data/학교,학과별 데이터셋/2023년_상반기_시도별.xlsx')
+school2019 = pd.ExcelFile('../../data/학교,학과별 데이터셋_전처리/2019년_상반기_시도별.xlsx')
+school2020 = pd.ExcelFile('../../data/학교,학과별 데이터셋_전처리/2020년_상반기_시도별.xlsx')
+school2021 = pd.ExcelFile('../../data/학교,학과별 데이터셋_전처리/2021년_상반기_시도별.xlsx')
+school2022 = pd.ExcelFile('../../data/학교,학과별 데이터셋_전처리/2022년_상반기_시도별.xlsx')
+school2023 = pd.ExcelFile('../../data/학교,학과별 데이터셋_전처리/2023년_상반기_시도별.xlsx')
 
 # 학교별 데이터 연도별 데이터 프레임 생성 - 시도별
 school2019_df = schooldf1(school2019)
